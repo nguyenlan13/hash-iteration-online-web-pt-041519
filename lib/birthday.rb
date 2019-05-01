06d867e71a26e0e0633014e78c486dbd98f0595b
Lan Nguyen <nguyenlan13@gmail.com>
2019-05-01 05:14:26 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -17,6 +17,7 @@ def age_appropriate_birthday(birthday_kids)
   birthday_kids.each do |name, age|
     if age <12
     puts "Happy Birthday #{name}! You are now #{age} years old!"
+  else nil
   end
   end
 end
